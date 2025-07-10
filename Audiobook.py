ee3be355418c337fd7caf4d2accc51ac117fce3a
adi785 <84303893+adi785@users.noreply.github.com>
2025-07-10 15:36:48 +0530

    Add files via upload

---
+++ Audiobook.py
@@ -0,0 +1,15 @@
+import pyttsx3
+from pypdf import PdfReader
+
+
+loct=("Kindly Import the pdf for audiobook: ")
+
+reader = PdfReader('loct')
+
+a=int(input("Enter the page number to start reading from: "))
+page= reader.pages[a]
+text = page.extract_text()
+
+speaker = pyttsx3.init()
+speaker.say(text)
+speak.runAndWait()  
