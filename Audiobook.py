219e36fd3cf4346ee83a00a1093daac57146c20f
adi785 <84303893+adi785@users.noreply.github.com>
2025-07-10 15:40:01 +0530

    Audiobook.py

--- Audiobook.py
+++ Audiobook.py
@@ -2,9 +2,9 @@ import pyttsx3
 from pypdf import PdfReader
 
 
-loct=("Kindly Import the pdf for audiobook: ")
+loct=input("Kindly Import the pdf for audiobook: ")
 
-reader = PdfReader('loct')
+reader = PdfReader(loct)
 
 a=int(input("Enter the page number to start reading from: "))
 page= reader.pages[a]
